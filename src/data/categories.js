d0a42b941205e24f49d26882857b35ccc5f62cea
DenViso <itdeniro00@gmail.com>
2023-09-11 21:00:44 +0300

    styling without order cart

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,14 +1,47 @@
 export const categories = [
   {
-    title: "menus",
+    title: "Burger",
     id: "sku_id_1",
+    img:"./img/pic/1.png"
   },
   {
-    title: "pizza",
+    title: "Snacks",
     id: "sku_id_2",
+    img:"./img/pic/2.png"
   },
   {
-    title: "kebab",
+    title: "Hot-Dogs",
     id: "sku_id_3",
+    img:"./img/pic/3.png"
+  },
+  {
+    title: "Kombo",
+    id: "sku_id_4",
+    img:"./img/pic/4.png"
+  },
+  {
+    title: "Shawarma",
+    id: "sku_id_5",
+    img:"./img/pic/5.png"
+  },
+  {
+    title: "Pizza",
+    id: "sku_id_6",
+    img:"./img/pic/6.png"
+  },
+  {
+    title: "Wok",
+    id: "sku_id_7",
+    img:"./img/pic/7.png"
+  },
+  {
+    title: "Desserts",
+    id: "sku_id_8",
+    img:"./img/pic/8.png"
+  },
+  {
+    title: "Sauces",
+    id: "sku_id_9",
+    img:"./img/pic/9.png"
   },
 ];
