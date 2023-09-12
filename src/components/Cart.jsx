e6a1b66341d72c7c2c3ce4bfb6179f713596f71a
DenViso <itdeniro00@gmail.com>
2023-09-12 22:32:49 +0300

    problem with delete cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,50 +1,79 @@
-import React from "react";
+
 import { useSelector } from "react-redux";
 import { products } from "../data/products";
+import {
+  addQuantity,
+  deletCartItem,
+  increaseQuantity,
+} from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
 
 export const Cart = () => {
+  const dispatch = useDispatch();
+  // const productId = useSelector((state) => state.modal.productId);
+  // const neededProduct = products.find((product) => product.id === productId);
   const cart = useSelector((state) => state.cart);
-
-  const offseting = () => {
-    cart.cartItems.pop();
+  if (cart.quantity < 1) {
+    dispatch(deletCartItem(cart.cartItems));
   }
-  console.log(cart.cartItems.length);
-  console.log(cart);
+  console.log(cart.quantity);
 
   return (
     <section className="cart-container">
       <div className="cart">
         <div className="cart-title">
-          <h1>Your order:<span>{cart.cartItems.length}</span></h1>
-          {/* <span className="cart-count">{cart.amount}</span> */}
+          <h1>Your order:</h1>
+          {/* <span>{cart.cartItems.length}</span> */}
+          <span className="cart-count">{cart.amount}</span>
         </div>
 
-        <div className="cart-items">
-          
+        <div className="cart-items" key={cart.cartItems}>
           {cart.cartItems.map((cartItem) => {
-            
             const neededItem = products.find(
               (product) => product.id === cartItem.productId
             );
-            
-            return (
 
+            return (
               <div className="cart-item" key={neededItem.productId}>
+                {/* photo */}
                 <img src={neededItem.img} alt="" />
-                <h3>{neededItem.title}</h3>
-                <h4>{neededItem.price}$</h4>
+                {/* info */}
+                <div className="cart-item-info">
+                  <div className="cart-item-info-title">
+                    <h3>{neededItem.title}</h3>
+                    <span>{neededItem.weight}g</span>
+                  </div>
+                  <h4>{neededItem.price}$</h4>
+                </div>
+                {/* button,count */}
                 <div className="cart-item-count">
-                  <button onClick={()=> offseting()}>-</button>
-                  <span>{cartItem.amount}</span>
-                  <button>+</button>
+                  <button
+                    onClick={() =>
+                      dispatch(increaseQuantity(cart.quantity - 1))
+                    }
+                  >
+                    -
+                  </button>
+
+                  <span>{cart.quantity}</span>
+
+                  <button
+                    onClick={() => dispatch(addQuantity(cart.quantity + 1))}
+                  >
+                    +
+                  </button>
+                </div>
+                <br />
+                {/* total */}
+                <div className="total-conteiner">
+                  <h2 className="cart-total">
+                    Total: <b>{neededItem.price * cart.quantity}$</b>
+                  </h2>
                 </div>
               </div>
             );
           })}
         </div>
-        <h2 className="cart-total">
-          Total: <b>{cart.total}$</b>
-        </h2>
       </div>
     </section>
   );
