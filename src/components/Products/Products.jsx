0156bc39149b36978408ab02ebb6d1618eb8887b
DenViso <itdeniro00@gmail.com>
2023-09-23 08:47:34 +0300

    Task-011: Fix main layout

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,14 +5,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { categories } from "../../data/categories";
 import "./productStyling.css";
 
-export const Products = ({category}) => {
+export const Products = ({ category }) => {
   const dispatch = useDispatch();
   const isOpen = useSelector((state) => state.modal.isOpen);
   console.log(category);
 
   return (
-    <>
-    
     <section className="products-container">
       <h1>{categories.find((cat) => cat.id === category).title}</h1>
       <div className="products">
@@ -21,11 +19,7 @@ export const Products = ({category}) => {
           .map((product) => {
             return (
               <div className="product-item" key={product.id}>
-                <img
-                  src={product.img}
-                  alt=""
-                  
-                />
+                <img src={product.img} alt="" />
                 <h2>{product.price}$</h2>
                 <h3>{product.title}</h3>
                 <span>{product.weight}g</span>
@@ -37,6 +31,5 @@ export const Products = ({category}) => {
           })}
       </div>
     </section>
-    </>
   );
 };
