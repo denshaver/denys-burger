b6f4bf68745bda57d15f44de15f27d4dff522fe6
DenViso <itdeniro00@gmail.com>
2023-09-26 18:03:29 +0300

    functionality with a basket

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { products } from "../../data/products";
 
 const initialState = {
   cartItems: [],
@@ -6,7 +7,7 @@ const initialState = {
   total: 0,
 };
 
-// { productId: "", amount: 2 }
+// { productId = "", amount = 2 };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -15,30 +16,39 @@ const cartSlice = createSlice({
     addItem: (state, action) => {
       state.cartItems.push(action.payload);
     },
+
+    deletCartItem: (state, action) => {
+      const deletedItem = state.cartItems.find(
+        (item) => item.productId === action.payload
+      );
+      state.cartItems = state.cartItems.filter(
+        (item) => item.productId !== action.payload
+      );
+    },
+
     increaseProduct: (state, action) => {
       const neededProduct = state.cartItems.find(
-        (product) => product.id === action.payload
+        (product) => product.productId === action.payload
       );
       neededProduct.amount += 1;
     },
+
     decreaseProduct: (state, action) => {
       const neededProduct = state.cartItems.find(
-        (product) => product.id === action.payload
+        (product) => product.productId === action.payload
       );
       neededProduct.amount -= 1;
     },
-    calcCartInfo: (state, action) => {
+
+    calcCartInfo: (state) => {
       let total = 0;
       let quantity = 0;
       state.cartItems.forEach((item) => {
-        total += item.price;
-        quantity += item.quantity * item.amount;
+        total += item.price * item.amount;
+        quantity += item.amount;
       });
       state.total = total;
       state.quantity = quantity;
-    },
-    deletCartItem: (state) => {
-      state.cartItems = [];
     },
   },
 });
