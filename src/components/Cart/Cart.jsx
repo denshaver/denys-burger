0f42479c4d5c2bd5031115e0005bd63fb5c8ec0f
VitaNomSmile <VNomerchyk@gmail.com>
2023-09-29 19:23:21 +0300

    Fix cart layout

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import {
   deletCartItem,
   increaseQuantity,
 } from "../../features/cart/cartSlice";
-import "./cartStyling.css"
+import "./cartStyling.css";
 
 export const Cart = () => {
   const dispatch = useDispatch();
@@ -22,71 +22,76 @@ export const Cart = () => {
 
   return (
     <section className="cart-container">
-      <div className="cart">
-
-        {!hasItemsInCart || cart.quantity < 1 ? (
+      {!hasItemsInCart || cart.quantity < 1 ? (
+        <div className="cart">
           <div className="cart-title">
-          <p className="cart-empty">
-
-            Your cart is empty. <br /> <span>place your order...</span>
-          </p>
+            <div className="cart-count">
+              <h3>Your order:</h3>
+              <button>{cart.quantity}</button>
+            </div>
+            <p>place your order...</p>
           </div>
-        ) : (
-          <div className="cart-title">
-            <h1>Your order:</h1>
-            {/* <span>{cart.cartItems.length}</span> */}
-            <span className="cart-count">{cart.quantity}</span>
-          </div>
-        )}
-
-        <div className="cart-items" key={products.id}>
-          {cart.cartItems.map((cartItem) => {
-            const neededItem = products.find(
-              (product) => product.id === cartItem.productId
-            );
-
-            return cart.quantity !== 0 ? (
-              <div className="cart-item" key={!neededItem.productId}>
-                {/* photo */}
-                <img src={neededItem.img} alt="" />
-                {/* info */}
-                <div className="cart-item-info">
-                  <div className="cart-item-info-title">
-                    <h3>{neededItem.title}</h3>
-                    <span>{neededItem.weight}g</span>
-                  </div>
-                  <h4>{neededItem.price}$</h4>
-                </div>
-                {/* button,count */}
-                <div className="cart-item-count">
-                  <button
-                    onClick={() =>
-                      dispatch(increaseQuantity(cart.quantity - 1))
-                    }
-                  >
-                    -
-                  </button>
-
-                  <span>{cart.quantity}</span>
-
-                  <button
-                    onClick={() => dispatch(addQuantity(cart.quantity + 1))}
-                  >
-                    +
-                  </button>
-                </div>
-                <br />
-                {/* total */}
-                <div className="total-conteiner">
-                  <h2 className="cart-total">
-                    Total: <b>{neededItem.price * cart.quantity}$</b>
-                  </h2>
-                </div>
-              </div>
-            ) : null;
-          })}
         </div>
-      </div>
+      ) : (
+        <div className="cart">
+          <div className="cart-title">
+            <div className="cart-count">
+              <h3>Your order:</h3>
+              <button>{cart.quantity}</button>
+            </div>
+          </div>
+
+          <div className="cart-items" key={products.id}>
+            {cart.cartItems.map((cartItem) => {
+              const neededItem = products.find(
+                (product) => product.id === cartItem.productId
+              );
+
+              return cart.quantity !== 0 ? (
+                <div className="cart-item" key={!neededItem.productId}>
+                  <div className="cart-item__info">
+                    <img src={neededItem.img} alt="" />
+                    <div className="item-info__block">
+                      <h4 className="item-title">{neededItem.title}</h4>
+                      <span className="weight">{neededItem.weight}g</span>
+                      <p className="price">{neededItem.price}$</p>
+                    </div>
+                  </div>
+                  <div className="cart-item__count">
+                    <button
+                      onClick={() =>
+                        dispatch(increaseQuantity(cart.quantity - 1))
+                      }
+                    >
+                      -
+                    </button>
+
+                    <span>{cart.quantity}</span>
+
+                    <button
+                      onClick={() => dispatch(addQuantity(cart.quantity + 1))}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+              ) : null;
+            })}
+          </div>
+          <div className="cart-total">
+            <div className="total-info">
+              <p>Total</p>
+              <p>300 $</p>
+            </div>
+            <button>Place an order</button>
+            <div className="cart-delivery">
+              <img src="/img/logo/free-icon-delivery-2362252.svg" alt="" />
+
+              <p>Free shipping</p>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
