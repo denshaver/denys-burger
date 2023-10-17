9d83cbcd5b9ee07cc61eb12f909d8fb99cbe55eb
DenViso <itdeniro00@gmail.com>
2023-10-17 13:11:51 +0300

    add close modalWindow

---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Order = () => {
+  return (
+   <div className="order-conteiner">
+    <div className="order-"></div>img
+    <div className="order-form">
+      
+    </div>
+   </div>
+  )
+}
