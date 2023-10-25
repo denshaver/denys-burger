d54b5f31d0a6e830188b7741ca9f4e2deb230f73
VitaNomSmile <VNomerchyk@gmail.com>
2023-10-26 00:28:13 +0300

    Fixed folded cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,7 +7,7 @@ import {
   deleteProduct,
 } from "../../features/cart/cartSlice";
 import "./cartStyling.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Cart = ({ t }) => {
   const dispatch = useDispatch();
@@ -19,88 +19,87 @@ export const Cart = ({ t }) => {
     dispatch(calcCartInfo());
   }, [cart]);
 
-  // if (cart.cartItems.find((item) => item.amount < 1))
-  // dispatch(deleteProduct(cart.cartItems.find((item) => item.amount < 1)));
-
-  console.log(cart.cartItems);
+  const [isCartFolded, setIsCartFolded] = useState(false);
 
   return (
-      <section className="cart-container">
-        {!hasItemsInCart || cart.quantity < 1 ? (
-          <div className="cart">
-            <div className="cart-title">
-              <div className="cart-count">
-                <h3>{t("cart.empty")}</h3>
-                <button>{cart.quantity}</button>
-              </div>
-              <p>{t("cart.emptySpan")}</p>
+    <section className="cart-container" >
+      {!hasItemsInCart || cart.quantity < 1 ? (
+        <div className="cart">
+          <div className="cart-title">
+            <div className="cart-count">
+              <h3>{t("cart.empty")}</h3>
+              <button>{cart.quantity}</button>
+            </div>
+            <p>{t("cart.emptySpan")}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="cart">
+          <div className="cart-title" onClick={() => {if(!isCartFolded) setIsCartFolded(true)}}>
+            <div className="cart-count">
+              <h3>{t("cart.order")}</h3>
+              <button>{cart.quantity}</button>
             </div>
           </div>
-        ) : (
-          <div className="cart">
-            <div className="cart-title">
-              <div className="cart-count">
-                <h3>{t("cart.order")}</h3>
-                <button>{cart.quantity}</button>
-              </div>
-            </div>
 
-            <div className="cart-items" key={products.id}>
-              {cart.cartItems.map((cartItem) => {
-                const neededItem = products.find(
-                  (product) => product.id === cartItem.productId
-                );
+          <div className={isCartFolded ? "cart-items" : "cart-items hidden"} key={products.id}>
+            {cart.cartItems.map((cartItem) => {
+              const neededItem = products.find(
+                (product) => product.id === cartItem.productId
+              );
 
-                return cart.quantity !== 0 ? (
-                  <div className="cart-item" key={neededItem.productId}>
-                    <div className="cart-item__info">
-                      <img src={neededItem.img} alt="" />
-                      <div className="item-info__block">
-                        <h4 className="item-title">{neededItem.title}</h4>
-                        <span className="weight">{neededItem.weight}g</span>
-                        <p className="price">{neededItem.price}$</p>
-                      </div>
-                    </div>
-                    <div className="cart-item__count">
-                      <button
-                        onClick={() => {
-                          if (cartItem.amount <= 1)
-                            dispatch(deleteProduct(cartItem.productId));
-                          dispatch(decreaseProduct(cartItem.productId));
-                        }}
-                      >
-                        -
-                      </button>
-
-                      <span>{cartItem.amount}</span>
-
-                      <button
-                        onClick={() =>
-                          dispatch(increaseProduct(cartItem.productId))
-                        }
-                      >
-                        +
-                      </button>
+              return cart.quantity !== 0 ? (
+                <div className="cart-item" key={neededItem.productId}>
+                  <div className="cart-item__info">
+                    <img src={neededItem.img} alt="" />
+                    <div className="item-info__block">
+                      <h4 className="item-title">{neededItem.title}</h4>
+                      <span className="weight">{neededItem.weight}g</span>
+                      <p className="price">{neededItem.price}$</p>
                     </div>
                   </div>
-                ) : null;
-              })}
-            </div>
-            <div className="cart-total">
-              <div className="total-info">
-                <p>{t("cart.total")}</p>
-                <p>{cart.total} $</p>
-              </div>
-              <button>{t("cart.orderBut")}</button>
-              <div className="cart-delivery">
-                <img src="/img/logo/free-icon-delivery-2362252.svg" alt="" />
+                  <div className="cart-item__count">
+                    <button
+                      onClick={() => {
+                        if (cartItem.amount <= 1)
+                          dispatch(deleteProduct(cartItem.productId));
+                        dispatch(decreaseProduct(cartItem.productId));
+                      }}
+                    >
+                      -
+                    </button>
 
-                <p>{t("cart.FreeShipping")}</p>
-                {/* <span>Fold</span> */}
-              </div>
+                    <span>{cartItem.amount}</span>
+
+                    <button
+                      onClick={() =>
+                        dispatch(increaseProduct(cartItem.productId))
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+              ) : null;
+            })}
+            <div className="cart-total">
+            <div className="total-info">
+              <p>{t("cart.total")}</p>
+              <p>{cart.total} $</p>
+            </div>
+            <button>{t("cart.orderBut")}</button>
+            <div className="cart-delivery">
+              <p>
+                <img src="/img/logo/free-icon-delivery-2362252.svg" alt="" />
+                {t("cart.FreeShipping")}
+              </p>
+              <span onClick={() => setIsCartFolded(false)}>Fold</span>
             </div>
           </div>
-        )}
-      </section>
+          </div>
+          
+        </div>
+      )}
+    </section>
   );
 };
